d10d6680ab658216ec1e180e8be674ce74d127af
Rohan Bamania <bamania.rohan@outlook.com>
2024-04-25 00:09:28 +0100

    Learned Basic Varibles and Data Types

---
+++ BasicVariablesAndDataTypes.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+int main()
+
+{
+	//Integer
+
+	int x = 5; 
+
+	int y = 6;
+	
+	int age = 21;
+
+	int year = 2023;
+
+	
+	//Double
+
+	double days = 7.5;
+
+	double price = 10.99;
+
+	double GPA = 2.5;
+
+
+	//Character
+
+	char grade = 'A';
+
+	char initial = 'B';
+
+	char currency = '£';
+
+
+	//Boolean
+
+	bool student = true;
+	
+	bool power = false;
+
+	bool forSale = true;
+
+
+
+	//String
+
+	std::string name = "Tyler";
+
+	std::string day = "Tuessday";
+
+	std::string food = "Burger";
+
+	std::string address = "5434 10th Rd W";
+
+
+
+	std::cout << "Hello " << name << std::endl;
+
+	std::cout << "You are " << age << " years old" << std::endl;
+
+
+	std::cin.get();
+
+}
