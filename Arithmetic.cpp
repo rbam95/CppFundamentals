90d77acf69df858b25c313df670f0ec6aa40130c
Rohan Bamania <bamania.rohan@outlook.com>
2024-04-26 02:18:00 +0100

    Arithmetic and Input

---
+++ Arithmetic.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+
+{
+	
+	//Addition
+
+	int student = 20;
+
+	student += 1;
+
+	std::cout << student << std::endl;
+
+	std::cin.get() >> student;
+
+	std::cout << "this is the new number " << student << std::endl;
+
+
+	//Subtraction
+
+	
+
+}
